a47939f8d05910c56dcaeecac90e22cf8bc9de00
WilfrenDeveloper <https://github.com/WilfrenDeveloper/e-commerce-whit-React.git>
2023-11-24 21:30:47 -0500

    advances

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,9 @@ function App() {
         <Route path='/register' element={<RegisterPage />} />
         <Route path='/login' element={<LoginPage />} />
       </Routes>
+      <div className='app__copy'>
+        <p className='app__p'>create for Wilfren Quintero</p>
+      </div>
     </div>
   )
 }
