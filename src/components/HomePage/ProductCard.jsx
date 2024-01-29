5fe9279c0a9c6c6905a74b478cd52ddc2404e9a8
WilfrenDeveloper <https://github.com/WilfrenDeveloper/e-commerce-whit-React.git>
2024-01-28 19:33:31 -0500

    advances

--- src/components/HomePage/ProductCard.jsx
+++ src/components/HomePage/ProductCard.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom'
-import '../../style/ProductCard.css'
+import './styles/ProductCard.css'
 import { addProductToCartThunk } from '../../store/slices/cart.slice'
 import { useDispatch } from 'react-redux'
 
@@ -20,14 +20,15 @@ const ProductCard = ({ product }) => {
     return (
         <article className='productcard__article' onClick={handleNavegate}>
             <header className='productcard__header'>
-                <img className='productcard__img' src={product.images[0]?.url} alt="" />
+                <img className='productcard__img product_img-1' src={product.images[1]?.url} alt="" />
+                <img className='productcard__img product_img-2' src={product.images[0]?.url} alt="" />
             </header>
             <section className='productcard__section'>
                 <h4 className='productcard__h4'>{product.brand}</h4>
                 <h3 className='productcard__h3'>{product.title}</h3>
                 <div className='productcard__div'>
-                    <span className='productcard__span productcard__span--label'>Price</span>
-                    <span className='productcard__span productcard__span--value'>{product.price}</span>
+                    <span className='productcard__span productcard__value--label'>Price</span>
+                    <span className='productcard__span productcard__value--value'>{product.price}</span>
                 </div>
                 <button className='productcard__button' onClick={handleAddCart}>
                     <i className='bx bx-cart productcard__bx'></i>
